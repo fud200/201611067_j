7d67d78ad7f98a6ffe39d6a0acf31a8d670d20bd
fud200 <fud1020@naver.com>
2016-10-17 12:23:14 +0900

    w3main 제출이름 변경
    w3main 제출이름 변경

---
+++ BrazilCoffee.java
@@ -0,0 +1,14 @@
+﻿class BrazilCoffee{
+	String origin;
+	int degreeOfRoast;
+	int grade;
+	public BrazilCoffee(String o){
+	this.origin=o;
+	}
+	public void roast(){
+	System.out.println("roasting..." +origin);
+	}
+	}
+
+BrazilCoffee c=new BrazilCoffee("Cenya");
+c.roast();
