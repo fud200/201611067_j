e15afaf33ccd543d2b17bba2d1ec470cc37b780c
fud200 <fud1020@naver.com>
2016-10-18 23:02:33 +0900

    사각형그리기과제
    사각형그리기과제

---
+++ DrawSquare.java
@@ -0,0 +1,11 @@
+﻿System.out.println("homework");
+/classpath lib/aplu5.jar
+import ch.aplu.turtle.*;
+Turtle t1=new Turtle();
+void DrawSquare(int size){
+	for(int i=0;i<4;i++){
+		t1.fd(size);
+		t1.rt(90);
+	}
+}
+DrawSquare(50)
