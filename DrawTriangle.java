43a50cc991d0786ef0dc0fe06d4f1ce2c8121884
fud200 <fud1020@naver.com>
2016-10-18 23:03:08 +0900

    삼각형그리기과제
    삼각형그리기과제

---
+++ DrawTriangle.java
@@ -0,0 +1,11 @@
+﻿System.out.println("homework");
+/classpath lib/aplu5.jar
+import ch.aplu.turtle.*;
+Turtle t1=new Turtle();
+void DrawTriangle(int size){
+	for(int i=0;i<3;i++){
+		t1.fd(size);
+		t1.rt(120);
+	}
+}
+DrawTriangle(60)
