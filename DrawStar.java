74ea35d792123d004d6a1d0b9d548bd34d423377
fud200 <fud1020@naver.com>
2016-10-18 23:03:00 +0900

    별그리기과제
    별그리기과제

---
+++ DrawStar.java
@@ -0,0 +1,11 @@
+﻿System.out.println("homework");
+/classpath lib/aplu5.jar
+import ch.aplu.turtle.*;
+Turtle t1=new Turtle();
+void DrawStar(int size){
+	for(int i=0;i<5;i++){
+		t1.fd(size);
+		t1.rt(144);
+	}
+}
+DrawStar(70)
