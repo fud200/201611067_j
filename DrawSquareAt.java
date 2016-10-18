43843efb84e8c7cebaec012111671055d1f3409d
fud200 <fud1020@naver.com>
2016-10-18 23:02:51 +0900

    지정된위치에가서사각형그리기과제
    지정된위치에가서사각형그리기과제

---
+++ DrawSquareAt.java
@@ -0,0 +1,12 @@
+﻿System.out.println("homework");
+/classpath lib/aplu5.jar
+import ch.aplu.turtle.*;
+Turtle t1=new Turtle();
+void DrawSquareAt(int size,double x,double y){
+	t1.setPos(x,y);
+	for(int i=0;i<4;i++){
+		t1.fd(size);
+		t1.rt(90);
+	}
+}
+DrawSquareAt(50,-30,-40)
