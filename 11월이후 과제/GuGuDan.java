56b2c3e1085104cf80ae4809611e4f98c3fc658d
fud200 <fud1020@naver.com>
2016-11-05 20:17:11 +0900

    구구단과제
    구구단과제

--- 11월이후 과제/GuGuDan.java
+++ 11월이후 과제/GuGuDan.java
@@ -2,7 +2,7 @@ public class GuGuDan {
  
     void gugudan() {
         for(int i=1; i<=9; i++) {
-            System.out.print(i+"´Ü- ");
+            System.out.print(i+"DAN- ");
 			for(int j=1; j<=9; j++) {
 				System.out.printf("%d * %d = %2d", i, j, (j*i));
                 System.out.print("\t");
