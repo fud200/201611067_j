2838ec65922768ac478b1b46575c2fc466c34e0a
fud200 <fud1020@naver.com>
2016-11-23 09:36:32 +0900

    Selection
    Selection

---
+++ 11월이후 과제/Selection.java
@@ -0,0 +1,15 @@
+package com.sd.rectangle;
+class Selection{
+  public void select(Rectangle r){
+    r.setSelected(true);
+    System.out.println("selecte: "+r.isSelected);
+  }
+    
+  public static void main(String[] args){
+    Rectangle rc = new Rectangle(20,30);
+    Cube cb = new Cube(11.0,21.0,31.0);
+    Selection s = new Selection();
+    s.select(rc);
+    s.select(cb);
+  }
+}
