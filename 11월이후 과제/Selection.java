9c029e84c8626207e976148e5e89bb516c8b3063
fud200 <fud1020@naver.com>
2016-11-26 21:09:42 +0900

    Selection
    Selection

--- 11월이후 과제/Selection.java
+++ 11월이후 과제/Selection.java
@@ -1,15 +1,24 @@
 package com.sd.ch8;
-class Selection{
-  public void select(Rectangle r){
-    r.setSelected(true);
-    System.out.println("selecte: "+r.isSelected);
-  }
-    
-  public static void main(String[] args){
-    Rectangle rc = new Rectangle(20,30);
-    Cube cb = new Cube(11.0,21.0,31.0);
-    Selection s = new Selection();
-    s.select(rc);
-    s.select(cb);
-  }
+import java.util.*;
+public class Selection {
+    ArrayList<Rectangle> sArr = new ArrayList<Rectangle>();
+    Selection(){}
+    public void select(Rectangle r) {
+        r.setSelected(true);
+        sArr.add(r);
+        System.out.println("You select "+r.isSelected);
+    }
+    public void print(){
+        for(Rectangle r:sArr){
+            System.out.println(r.toString());
+        }
+    }
+    public static void main(String[] args){
+    	Selection s = new Selection();
+    	Rectangle r = new Rectangle(20,40);
+        Cube c = new Cube(20,30,40);
+		s.select(r);
+        s.select(c);
+        s.print();
+    }
 }
