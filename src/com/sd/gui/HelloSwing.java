ce774176c5ae85286166991135f9f681678f2cd6
fud200 <fud1020@naver.com>
2016-12-07 02:00:51 +0900

    HelloSwing
    HelloSwing

---
+++ src/com/sd/gui/HelloSwing.java
@@ -0,0 +1,11 @@
+package com.sd.gui;
+
+import javax.swing.JFrame;
+
+public class HelloSwing extends JFrame {
+	public HelloSwing() {
+		setTitle("Hello");
+		setSize(400, 400);
+		setVisible(true);
+	}
+}
